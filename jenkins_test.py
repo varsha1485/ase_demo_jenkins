025d5f9fd40d97a372a6682b7c64ad30aa6203a2
varsha1485 <120306864+varsha1485@users.noreply.github.com>
2025-07-03 15:48:05 +0530

    Update jenkins_test.py

--- jenkins_test.py
+++ jenkins_test.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a number: "))
 if num > 1:
-    for i in range(2, num):
+    for i in range(4, num):
         if num % i == 0:
             print("Not Prime")
             break
